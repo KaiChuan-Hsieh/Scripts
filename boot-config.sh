8df14a509380b7045e90d8568cfe8d47079c7f62
Kirt Hsieh <kirt_hsieh@asus.com>
2016-05-13 11:26:53 +0800

    unify script style

--- boot-config.sh
+++ boot-config.sh
@@ -3,12 +3,12 @@
 HOST=$1
 
 case $1 in
-"kirt-server")
-mount /dev/sdb ~/Disk2
-ip route add 10.72.71.0/24 dev eth1
-ip route add 10.193.115.0/24 dev eth1
-;;
-*)
-echo "Input host PC name as parameter!!"
-;;
+    "kirt-server")
+        mount /dev/sdb ~/Disk2
+        ip route add 10.72.71.0/24 dev eth1
+        ip route add 10.193.115.0/24 dev eth1
+        ;;
+    *)
+        echo "Input host PC name as parameter!!"
+        ;;
 esac
