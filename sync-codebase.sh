a16162c78f1386c8b0e57371d2e0ef99b3903dca
Kirt Hsieh <kirt_hsieh@asus.com>
2016-05-13 11:23:24 +0800

    update sync script
    1. change name from partner-sync.sh to sync-codebase.sh
    2. add m-mr1-r2_cht_hr into script

--- sync-codebase.sh
+++ sync-codebase.sh
@@ -6,11 +6,16 @@ then
 else
     echo "Select a branch indicated below:"
     echo "[1] l-mr1-dev-fugu"
+    echo "[2] m-mr1-r2_cht_hr"
     read -p "Selection: " choice
 
     case $choice in
     "1") 
         repo init -u https://partner-android.googlesource.com/platform/vendor/pdk/fugu/aosp_fugu-userdebug/manifest -b l-mr1-dev-fugu
+        repo sync
+        ;;
+    "2")
+        repo init -u ssh://gerrit.corpnet.asus/platform/manifest.git -b m-mr1-r2_cht_hr
         repo sync
         ;;
     *) 
