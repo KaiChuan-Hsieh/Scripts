ebe9cf044674a53239c5667e9e5d5f7766499a0f
Kirt Hsieh <kirt_hsieh@asus.com>
2016-09-29 16:02:52 +0800

    rockchip-eeprom: Make SN size up to 20 bytes

--- rockchip-eeprom.sh
+++ rockchip-eeprom.sh
@@ -73,15 +73,31 @@ function write_ether {
 }
 
 function read_sn {
-    data=`xxd -s 0x6 -l 12 -g 1 $FILEPATH \
-        | awk '{ print $2$3$4$5$6$7$8$9$10$11$12$13 }'`
+    data1=`xxd -s 0x6 -l 16 -g 1 $FILEPATH \
+        | awk '{ print $2$3$4$5$6$7$8$9$10$11$12$13$14$15$16$17 }'`
+    data2=`xxd -s 0x16 -l 4 -g 1 $FILEPATH \
+        | awk '{ print $2$3$4$5 }'`
 
     idx=0
-    while [ $idx -lt 24 ];
+    while [ $idx -lt 32 ];
     do
-        val=${data:$idx:2}
-        tmp=$(printf "\x$val")
-        SN="$SN$tmp"
+        val=${data1:$idx:2}
+        if [ $val != "00" ]
+        then
+            tmp=$(printf "\x$val")
+            SN="$SN$tmp"
+        fi
+        idx=$((idx+2))
+    done
+    idx=0
+    while [ $idx -lt 8 ];
+    do
+        val=${data2:$idx:2}
+        if [ $val != "00" ]
+        then
+            tmp=$(printf "\x$val")
+            SN="$SN$tmp"
+        fi
         idx=$((idx+2))
     done
 
@@ -92,25 +108,27 @@ function read_sn {
 
 function write_sn {
     PARA=$1
-    if [ ${#PARA} -ne 12 ]; then
+    SNLEN=${#PARA}
+    if [ $SNLEN -gt 20 ]; then
         echo "invalid input"
         exit 1
     fi
+
+    dd if=/dev/zero of=$FILEPATH bs=1 count=20 seek=6 > /dev/null 2>&1
 
     touch sn.bin
     gpio 6 acquire
     gpio 6 output
 
     idx=0
-    while [ $idx -lt 12 ];
+    while [ $idx -lt $SNLEN ];
     do
         tmp=$(printf %02x \'${PARA:$idx:1})
-        data="$data$tmp"
         echo -n $tmp | xxd -r -p >> sn.bin
         idx=$((idx+1))
     done
 
-    dd if=sn.bin of=$FILEPATH bs=1 count=12 seek=6 > /dev/null 2>&1
+    dd if=sn.bin of=$FILEPATH bs=1 count=$SNLEN seek=6 > /dev/null 2>&1
     rm sn.bin
     gpio 6 input
     gpio 6 release
